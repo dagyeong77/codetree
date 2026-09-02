9a61fec5bb8aabdd51f88fa75fe121a5a23e2b2a
남다경 <dagyeongnam@gmail.com>
2026-09-02 22:02:44 +0900

    [Passed] 삼항연산자 (132ms, 10MB)

---
+++ trail1/삼항연산자/ternary-operator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int score = sc.nextInt();
+        String a;
+
+        a = score == 100 ? "pass" : "failure";
+
+        System.out.println(a);
+    }
+}
