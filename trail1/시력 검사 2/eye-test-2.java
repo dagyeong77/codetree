786cd0cd3d658364304f09b956ce4367266cb31c
남다경 <dagyeongnam@gmail.com>
2026-09-02 22:11:19 +0900

    [Passed] 시력 검사 2 (145ms, 11MB)

---
+++ trail1/시력 검사 2/eye-test-2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        double m = sc.nextDouble();
+
+        if (m>=1.0) {
+            System.out.println("High");
+        } else if (m>=0.5) {
+            System.out.println("Middle");
+        } else {
+            System.out.println("Low");
+        }
+    }
+}
