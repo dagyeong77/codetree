603bab4114aea37049b3ad5025d65d983a339047
남다경 <dagyeongnam@gmail.com>
2026-09-03 22:21:01 +0900

    [Passed] 홀수만 출력 2 (131ms, 10MB)

---
+++ trail1/홀수만 출력 2/output-only-odd-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int B = sc.nextInt();
+        int A = sc.nextInt();
+
+        for (int i=B; i>=A; i--) {
+            if(i%2 == 1) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
