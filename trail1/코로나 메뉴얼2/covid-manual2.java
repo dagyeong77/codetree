08ff54a9fdac4cb472b662f59d2a4b2566bf1ff2
남다경 <dagyeongnam@gmail.com>
2026-09-08 11:21:44 +0900

    [Passed] 코로나 메뉴얼2 (106ms, 9MB)

--- trail1/코로나 메뉴얼2/covid-manual2.java
+++ trail1/코로나 메뉴얼2/covid-manual2.java
@@ -1,34 +1,52 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        // Please write your code here.
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
 
-        int[] countArr = new int[5];
-        int type = 0;
-        char s;
-        int temp;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
+        //진료소에 보내지는 인원 수 세기 
+        int[] hos = new int[4];
+
+        //3명의 환자 상태 하나씩 검사 
         for(int i=0; i<3; i++) {
-            s = sc.next().charAt(0);
-            temp = sc.nextInt();
+            st = new StringTokenizer(br.readLine()); // st = ["Y". "38"]
+            String tempString = st.nextToken(); // tempString = "Y"
+            char cold = tempString.charAt(0);   // cold = 'Y'
+            //char cold = st.nextToken().charAt(0);
 
-            if(s == 'Y' && temp >= 37) {
-                type = 1;
-            } else if(temp >= 37) {
-                type = 2;
-            } else if(s == 'Y') {
-                type = 3;
+            int temp = Integer.parseInt(st.nextToken());
+ 
+            //감기 증상이 있는 경우 
+            if(cold == 'Y') {
+                //체온 구분 
+                if(temp >= 37) {
+                    hos[0]++;
+                } else {
+                    hos[2]++;
+                }
+            //감기 증상이 없는 경우 
             } else {
-                type = 4;
+                if(temp >= 37) {
+                    hos[1]++;
+                } else {
+                    hos[3]++;
+                }
             }
-            countArr[type]++;
         }
-        for(int i=1; i<=4; i++) {
-            System.out.print(countArr[i] + " ");
+
+        //반복문으로 진료소에 보내지는 인원 출력 
+        for(int i=0; i<4; i++) {
+            System.out.print(hos[i] + " ");
         }
-        if(countArr[1] >= 2) {
-            System.out.println("E");
+
+        //위급상황인 경우 
+        if(hos[0] >= 2) {
+            System.out.print('E');
         }
     }
 }
