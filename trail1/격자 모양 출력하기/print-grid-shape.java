f160df06ad221015327b61c75ab772fd00d0dd26
남다경 <dagyeongnam@gmail.com>
2026-09-09 10:31:02 +0900

    [Passed] 격자 모양 출력하기 (95ms, 9MB)

---
+++ trail1/격자 모양 출력하기/print-grid-shape.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        int[][] map = new int[N][N]; 
+        
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            r = r - 1;
+            c = c - 1;
+            map[r][c] = (r + 1) * (c + 1);
+        }
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
